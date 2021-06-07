ffc2bce24fe123b160e878dd1a422878a22eb294
Student <b1951514@debian>
2021-06-07 18:17:32 +0300

    1234112312

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ unsigned char *reflect(unsigned char *data, int x, int y, int n) {
 	for (int i = 0; i < y; ++i) {
 		for (int j = 0; j < x; ++j) {
 			for (int l = 0; l < n; ++l) {
-				data[i * x * n + j * n  + l] = data[  n * i * x +  n * j + 3*l ];
+				data[i * x * n + j * n  + l] = data[  n * i * x +  n * j + 5*l ];
 			}
 		}
 	}
